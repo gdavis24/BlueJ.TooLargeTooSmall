12f90b78a7f2c314d3de5dc67f936d71ac6f22b0
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-05 18:09:29 -0500

    uploading completed lab

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -15,10 +15,23 @@ public class TooLargeTooSmall {
         // initialise instance variables
         chosenNum = seed;
     }
-    
+
     public Integer guess(Integer g) {
-        
-        return chosenNum;
+        Integer result = 0;
+
+    if (chosenNum < g){
+      //This is when the guessed number is bigger than the chosen number
+      result = 1;
+    }
+    else if (chosenNum > g){
+      //This is when the guessed number is smaller than the chosen number
+      result = -1;
+    }
+    else {
+      //If other two fail, then chosent number should be equal to guessed number
+      result = 0;
+    }
+        return result;
 
     }
 }
